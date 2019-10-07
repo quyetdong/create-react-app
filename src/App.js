db9c725b7b258b4edea80d11883654ae2520e3b1
Quyet Dong Van <quyet.dv@temando.com>
2019-10-07 13:00:07 +0700

    updated settings create-react-app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function Hello() {
+function App() {
   return (
-    <div>
-      <h1>Welcome to React</h1>
-      <p>hello friend!</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Hello;
+export default App;
