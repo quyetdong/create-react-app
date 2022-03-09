0e37c90e053f7717fa822a2748a3c985137f8764
Kent Dong <kent@vinova.com.sg>
2022-03-09 18:37:24 +0700

    udpate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import Person from './components/Person';
+import People from './components/People';
 
 function App() {
-  const changeNameHandler = (e) => {
-    console.log(e);
-  }
-
+  
   return (
     <div className="App">
       <h1>This is my react app</h1>
       <p>Description of Application</p>
-      <button onClick={changeNameHandler}>Change name</button>
-      {/* <button onClick={() => changeNameHandler('ABC')}>Change name 2</button> */}
-      <Person name="Toan" age="28"/>
-      <Person name="Thanh" age="32"/>
-      <Person name="Big Man" age="10">My hobbies are: teaching, running</Person>
+      <People />
     </div>
   );
 }
---
+++ src/components/People.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Person from './Person';
+
+export default function People() {
+  const hobbies = ['Teaching', 'Running'];
+  const defaultPeople = [
+    { name: 'Tedu 1', age: 1, hobbies },
+    { name: 'Tedu 2', age: 2 },
+    { name: 'Tedu 3', age: 3 },
+  ];
+  const [people, setPeople] = useState(defaultPeople);
+
+  const UpdatePeopleHandler = (e) => {
+    console.log(e);
+
+    const updatedPeople = [
+      { name: people[1].name, age: people[1].age + 1, hobbies: people[1].hobbies }, 
+      { name: people[2].name, age: people[2].age + 1, hobbies: people[2].hobbies },
+      { name: people[0].name, age: people[0].age + 1, hobbies: people[0].hobbies }
+    ];
+    setPeople(updatedPeople);
+  }
+
+  return (
+    <div>
+      <Person name={people[0].name} age={people[0].age}>{`${people[0].hobbies ? 'My hobbies are: ' + people[0].hobbies.join(', ') : ''}`}</Person>
+      <Person name={people[1].name} age={people[1].age}>{`${people[1].hobbies ? 'My hobbies are: ' + people[1].hobbies.join(', ') : ''}`}</Person>
+      <Person name={people[2].name} age={people[2].age}>{`${people[2].hobbies ? 'My hobbies are: ' + people[2].hobbies.join(', ') : ''}`}</Person>
+      <button onClick={UpdatePeopleHandler}>Update People</button>
+      {/* <button onClick={() => UpdatePeopleHandler('ABC')}>Change name 2</button> */}
+    </div>
+  );
+}
